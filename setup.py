560316bf48500178f1f3c3db51ea9d22dae366bb
jack-heinzel <jack.heinzel@ligo.org>
2025-09-08 10:26:49 -0700

    add documentation yaml

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setuptools.setup(
     name='population-error',
     version='0.1',
     description='JAX-based package for estimating the information lost due to Monte Carlo approximations in GW population inference.',
-    url='#',
+    url='https://github.com/jack-heinzel/population-error',
+    project_urls={
+        'Source': 'https://github.com/jack-heinzel/population-error',
+        'Documentation': '',
+    },
     author='Jack Heinzel',
     install_requires=['jax', 'jax_tqdm', 'gwpopulation', 'bilby'],
     author_email='heinzelj@mit.edu',
