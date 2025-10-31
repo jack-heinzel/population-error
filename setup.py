26d8155493c6ebae0eaa7e2a2db480350a0a65e4
jack-heinzel <jack.heinzel@ligo.org>
2025-10-31 14:14:16 -0700

    add long description

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name='population-error',
     version='0.1',
     include_package_data=False,
     description='JAX-based package for estimating the information lost due to Monte Carlo approximations in GW population inference.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/jack-heinzel/population-error',
     project_urls={
         'Source': 'https://github.com/jack-heinzel/population-error',
